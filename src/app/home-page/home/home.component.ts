6fca80e707a455fade3a252b40018a84da7c4c20
Steve S. Lee <sjlee12@me.com>
2025-01-20 01:34:29 -0500

    added promo section

--- src/app/home-page/home/home.component.ts
+++ src/app/home-page/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatDividerModule, MatIconModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   standalone: true
