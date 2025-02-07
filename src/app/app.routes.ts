263c941e5d29a691d108b589f2241d32c45a97f8
Steve S. Lee <sjlee12@me.com>
2025-02-07 18:16:37 -0500

    added padding next to each column

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import {DashboardComponent} from "./dashboard/dashboard.component";
 import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 import { SkillsComponent } from './home-page/skills/skills.component';
-import { HomeComponent } from './home-page/home/home.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     { path: 'home', title: "App Home page", component: HomeComponent },
