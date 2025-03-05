d6eb908f4e358b24145b1cae66446631d7a79c82
Steve S. Lee <sjlee12@me.com>
2025-03-04 22:33:39 -0500

    finished exp section

--- src/app/home/exp/exp.component.ts
+++ src/app/home/exp/exp.component.ts
@@ -1,11 +1,11 @@
-import {ChangeDetectionStrategy, Component, viewChild} from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {provideNativeDateAdapter} from '@angular/material/core';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatAccordion, MatExpansionModule} from '@angular/material/expansion';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatIconModule} from '@angular/material/icon';
-import {MatInputModule} from '@angular/material/input';
+import { ChangeDetectionStrategy, Component, viewChild } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatAccordion, MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import experiences from './exp.content.json';
 import { Exp } from './exp.model';
@@ -36,12 +36,12 @@ export class ExpComponent {
     const exp: {} = {
       ...e,
       highlights: e.highlights.map(h => h as Highlight),
-      startDate: new Date(e.startDate),
-      endDate: new Date(e.endDate),
-      dates: `${new Date(e.startDate).toLocaleString("default", {month: "short"})} ${new Date(e.startDate).getFullYear()} 
-      - ${new Date(e.endDate).toLocaleString("default", {month: "short"})} ${new Date(e.endDate).getFullYear()}`
+      startDate: new Date(e.startDate).setHours(0, 0, 0, 0),
+      endDate: new Date(e.endDate).setHours(0, 0, 0, 0),
+      dates: `${new Date(e.startDate).toLocaleString("default", { month: "short" })} ${new Date(e.startDate).getFullYear()} 
+      - ${new Date(e.endDate).toLocaleString("default", { month: "short" })} ${new Date(e.endDate).getFullYear()}`
     };
     return exp as Exp;
-  }).sort((a, b) => b.endDate.getDate() - a.endDate.getDate());
-  
+  }).sort((a, b) => b.endDate.valueOf() - a.endDate.valueOf())
+
 }
